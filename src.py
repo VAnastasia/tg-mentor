628ac928002bfc7a94b80fe60b6133d1b7815c7f
guschina.aa <v.anastasiya26@mail.ru>
2025-10-24 23:06:41 +0700

    init

---
+++ src.py
@@ -0,0 +1,15 @@
+import os  # Импортируем стандартный модуль для работы с операционной системой и переменными окружения
+from dotenv import load_dotenv  # Импортируем функцию для загрузки переменных окружения из файла .env
+
+load_dotenv()  # Загружаем переменные окружения из файла .env в текущую рабочую среду
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")  # Получаем значение переменной окружения с API-ключом OpenAI
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  # Получаем токен для Telegram-бота
+PROXIAPI_BASE_URL = os.getenv("PROXIAPI_BASE_URL")
+
+ASSISTANT_ID = os.getenv("ASSISTANT_ID")  # Получаем идентификатор ассистента OpenAI
+GIGACHAT_CREDENTIALS = os.getenv("GIGACHAT_CREDENTIALS")  # Получаем учетные данные для GigaChat
+LANGFUSE_SECRET_KEY = os.getenv("LANGFUSE_SECRET_KEY")  # Получаем секретный ключ для сервиса Langfuse
+LANGFUSE_PUBLIC_KEY = os.getenv("LANGFUSE_PUBLIC_KEY")  # Получаем публичный ключ для сервиса Langfuse
+
+LANGFUSE_HOST = os.getenv("LANGFUSE_HOST")  # Получаем адрес или домен сервиса Langfuse
